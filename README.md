f3de05ba4a292209865a47e76f018f10bde1f02d
Stefan Bohacek <stefan@fourtonfish.com>
2021-04-27 23:57:41 -0400

    First commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+1. Download `MPVDatasetDownload.xlsx` from [mappingpoliceviolence.org](https://mappingpoliceviolence.org/).
+2. Open the file, remove unnecessary columns, export to `MPVDatasetDownload.csv`.
+3. Run `python data-cleanup.py` to create `data-summary.csv`.
---
+++ data-cleanup.py
@@ -0,0 +1,73 @@
+import csv
+import json
+
+# Helper function for retrieving the year from the dataset
+def get_year( item ):
+    return item["Year"]
+
+# Convert CSV files to a dictionary
+
+data = {}
+
+with open( "MPVDatasetDownload.csv", encoding = "utf-8" ) as csvf:
+    csvReader = csv.DictReader(csvf)
+    key = 0
+    for rows in csvReader:
+        key += 1
+        data[key] = rows
+
+stats = []
+statuses_charges = []
+
+for index in data:
+    year = "20" + data[index]["Date of Incident (month/day/year)"][-2:]
+
+    # Skip data for 2021 that's still in progress
+
+    if year != "2021":
+        years_processed = map( get_year, stats )
+
+        if year not in years_processed:
+            stats.append ( {
+                "Year": year,
+                "Murders": 0,
+                "Charges": 0,
+                "Convictions": 0
+            } )
+
+            current_index = len( stats ) - 1
+        else:
+            for i, item in enumerate( stats ):
+                if stats[i]["Year"] == year:
+                    current_index = i
+
+        if stats[current_index]["Charges"] not in statuses_charges:
+            statuses_charges.append( stats[current_index]["Charges"] )
+
+        stats[current_index]["Murders"] += 1
+
+        if "Charged" in data[index]["Criminal Charges?"]:
+            stats[current_index]["Charges"] += 1
+
+        if "Charged, Convicted" in data[index]["Criminal Charges?"]:
+            stats[current_index]["Convictions"] += 1
+
+stats.reverse()
+
+print( json.dumps( stats, indent = 4, sort_keys=True ) )
+
+# Save the dataset to a CSV file
+    
+data_file = open( "data-summary.csv", "w" )
+csv_writer = csv.writer( data_file )
+count = 0
+
+for item in stats:
+    if count == 0:
+        header = item.keys()
+        csv_writer.writerow( header )
+        count += 1
+  
+    csv_writer.writerow( item.values() )
+  
+data_file.close()
